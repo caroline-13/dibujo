9af51e0d5b2b22df575b7b0fae249bd890a7fe3b
odette <113144923+caroline-13@users.noreply.github.com>
2023-05-03 15:56:29 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+}
+
 function setup(){
     canvas=createCanvas(280,280);
     background("white");
+    canvas.mouseReleased(analizarDibujo);
 
 }
 function draw (){
@@ -9,4 +14,15 @@ function draw (){
     if(mouseIsPressed){
         line(pmouseX,pmouseY,mouseX,mouseY)
     }
+}
+function borrar(){
+    background("white");
+}
+function analizarDibujo(){
+    classifier.classify(canvas,resultado);
+}
+function resultado(error,respuesta){
+if(!error){
+    console.log(respuesta);
+}
 }
